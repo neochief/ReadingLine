d0523677fbb97e7450d4ee544f265a2776d56130
Alexander Shvets <neochief@shvetsgroup.com>
2017-08-23 15:43:41 +0300

    Initial commit.

--- reading-line-bg.js
+++ reading-line-bg.js
@@ -1,22 +1,78 @@
+var ReadingLine = localStorage.getItem("ReadingLine") === "true";
+
+function toggleIcon() {
+    chrome.tabs.query({currentWindow: true}, function (tabs) {
+        for (var i in chrome.tabs) {
+            if (ReadingLine) {
+                chrome.browserAction.setIcon({
+                    path: "icons/icon19-active.png",
+                    tabId: chrome.tabs[i].id
+                });
+            }
+            else {
+                chrome.browserAction.setIcon({
+                    path: "icons/icon19.png",
+                    tabId: chrome.tabs[i].id
+                });
+            }
+        }
+    });
+}
+
+toggleIcon();
+
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "toggle"}, function (response) {
-        });
+    chrome.tabs.query({currentWindow: true}, function (tabs) {
+        for (var i in tabs) {
+            chrome.tabs.sendMessage(tabs[i].id, {
+                action: ReadingLine ? "ReadingLine-disable" : "ReadingLine-enable"
+            });
+        }
     });
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.message === "enable") {
-        chrome.browserAction.setIcon({
-            path: "icons/icon19-active.png",
-            tabId: sender.tab.id
-        });
+    if (request.message === "ReadingLine-status") {
+        sendResponse(ReadingLine);
     }
 
-    if (request.message === "disable") {
-        chrome.browserAction.setIcon({
-            path: "icons/icon19.png",
-            tabId: sender.tab.id
-        });
+    if (request.message === "ReadingLine-enable") {
+        ReadingLine = true;
+        localStorage.setItem("ReadingLine", ReadingLine);
+        toggleIcon();
+    }
+
+    if (request.message === "ReadingLine-disable") {
+        ReadingLine = false;
+        localStorage.setItem("ReadingLine", ReadingLine);
+        toggleIcon();
+    }
+
+    if (request.message === "ReadingLine-ping") {
+        injectedScript[sender.tab.id] = true;
     }
 });
+
+var injectedScript = {};
+
+chrome.tabs.query({currentWindow: true}, function (tabs) {
+    for (var i in tabs) {
+        injectedScript[tabs[i].id] = null;
+        chrome.tabs.sendMessage(tabs[i].id, {action: "ReadingLine-ping"});
+    }
+
+    setTimeout(function() {
+        for (var tabId in injectedScript) {
+            tabId = parseInt(tabId);
+            if (!injectedScript[tabId]) {
+                chrome.tabs.insertCSS(tabId, {
+                    file: "reading-line.css"
+                });
+                chrome.tabs.executeScript(tabId, {
+                    file: "reading-line.js"
+                });
+            }
+        }
+    }, 1000)
+});
+
--- reading-line.js
+++ reading-line.js
@@ -3,24 +3,46 @@ var ReadingLine = {
     hidden: false,
     div: null,
     init: function () {
-        this.div = document.createElement('div');
-        this.div.id = 'ReadingLine';
+        // Cleanup existing div just in case.
+        var div = document.getElementById("ReadingLine");
+        if (div) {
+            document.body.removeChild(div);
+        }
+
+        this.div = document.createElement("div");
+        this.div.id = "ReadingLine";
 
         this.mouseMove = this.mouseMove.bind(this);
         this.mouseOut = this.mouseOut.bind(this);
         this.shortCut = this.shortCut.bind(this);
+        this.checkStatus = this.checkStatus.bind(this);
 
-        document.addEventListener('keydown', this.shortCut, false);
+        document.addEventListener("keydown", this.shortCut, false);
+
+        this.checkStatus();
+
+        // The interval will make sure the content script will get disabled on
+        // extension uninstall.
+        setInterval(this.checkStatus, 5000);
     },
-    toggle: function () {
-        if (!this.active) {
-            this.enable();
+    checkStatus: function(){
+        try {
+            chrome.runtime.sendMessage({"message": "ReadingLine-status"}, function (status) {
+                if (status) {
+                    this.enable();
+                }
+                else {
+                    this.disable();
+                }
+            }.bind(this));
         }
-        else {
+        catch (e) {
             this.disable();
         }
     },
     enable: function () {
+        if (this.active) return;
+
         document.body.appendChild(this.div);
         document.addEventListener("mousedown", this.mouseMove);
         document.addEventListener("mouseup", this.mouseMove);
@@ -28,33 +50,43 @@ var ReadingLine = {
         document.addEventListener("mouseover", this.mouseMove);
         document.addEventListener("mouseout", this.mouseOut);
         this.active = true;
-        chrome.runtime.sendMessage({"message": "enable"});
+        chrome.runtime.sendMessage({"message": "ReadingLine-enable"});
     },
     disable: function () {
-        document.body.removeChild(this.div);
+        if (!this.active) return;
+
+        var div = document.getElementById("ReadingLine");
+        if (div) {
+            document.body.removeChild(div);
+        }
         document.removeEventListener("mousedown", this.mouseMove);
         document.removeEventListener("mouseup", this.mouseMove);
         document.removeEventListener("mousemove", this.mouseMove);
         document.removeEventListener("mouseover", this.mouseMove);
         document.removeEventListener("mouseout", this.mouseOut);
         this.active = false;
-        chrome.runtime.sendMessage({"message": "disable"});
+        chrome.runtime.sendMessage({"message": "ReadingLine-disable"});
     },
     mouseMove: function (e) {
-        this.div.className = 'active';
-        this.div.style.top = (e.clientY) + 'px';
+        this.div.className = "active";
+        this.div.style.top = (e.clientY) + "px";
         this.hidden = false;
     },
     mouseOut: function (e) {
         if (e.toElement == null && e.relatedTarget == null) {
-            this.div.className = 'hidden';
+            this.div.className = "hidden";
             this.hidden = true;
         }
     },
-    shortCut: function(e) {
+    shortCut: function (e) {
         // Ctrl/Cmd + Alt + -
         if ((e.ctrlKey || e.metaKey) && e.altKey && e.keyCode == 189) {
-            this.toggle();
+            if (!this.active) {
+                this.enable();
+            }
+            else {
+                this.disable();
+            }
         }
     }
 };
@@ -62,7 +94,16 @@ var ReadingLine = {
 ReadingLine.init();
 
 chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
-    if (msg.action == 'toggle') {
-        ReadingLine.toggle();
+    switch (msg.action) {
+        case "ReadingLine-enable":
+            ReadingLine.enable();
+            break;
+        case "ReadingLine-disable":
+            ReadingLine.disable();
+            break;
+        case "ReadingLine-ping":
+            chrome.runtime.sendMessage({"message": "ReadingLine-ping"});
+            break;
     }
+    sendResponse(msg.data, true);
 });
